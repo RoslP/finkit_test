1d355cd86bc10d720885ffd48b61982c66ec5d22
pavel99 <ght12345ght12345@gmail.com>
2025-10-29 21:02:11 +1000

    Пагинатор

--- includes/core/class_user.php
+++ includes/core/class_user.php
@@ -77,14 +77,19 @@ class User {
                 'last_login' => date('Y/m/d', $row['last_login']),
             ];
         }
-
-        return ['items' => $items];
+        $q = DB::query("SELECT count(*) FROM users ".$where.";");
+        $count = ($row = DB::fetch_row($q)) ? $row['count(*)'] : 0;
+        $url = 'users?';
+        if ($search) $url .= '&search='.$search;
+        paginator($count, $offset, $limit, $url, $paginator);
+        // output
+        return ['items' => $items, 'paginator' => $paginator];
     }
 
     public static function users_fetch($d = [])
     {
         $items = User::users_list($d);
         HTML::assign('users', $items['items']);
-        return ['html' => HTML::fetch('./partials/users_table.html'), 'paginator' => 0];
+        return ['html' => HTML::fetch('./partials/users_table.html'), 'paginator' => $items['paginator']];
     }
 }
